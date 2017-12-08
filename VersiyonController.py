0c585ccdf87373ca91ad9b6e2a6959b6bad21375
okuldan <asd@asd.asd>
2017-12-08 15:09:32 +0300

    falan filan

--- VersiyonController.py
+++ VersiyonController.py
@@ -1,19 +1,32 @@
 import os
+from shutil import copy2
 class Dosya:
-    def __init__(self, ad):
-        self.ad = ad
-        self.kesin_yolu = os.path.abspath(self.ad)
+
+    def __init__(self, yol):
+        self.kesin_yolu = os.path.abspath(yol)
+        self.ad = os.path.split(self.kesin_yolu)[1]
         self.degisme_tarihi = os.path.getmtime(self.kesin_yolu)
 
-class Directory(Dosya):
-    def icindekiler(self):
-        sozluk = dict()
-        for dosya_adi in os.listdir(self.kesin_yolu):
-            buffer_list = [0,0]
-            buffer_list[0] = os.path.abspath(os.path.join(self.kesin_yolu, dosya_adi))
-            buffer_list[1] = os.path.getmtime(buffer_list[0])
-            sozluk[dosya_adi] = buffer_list
-        return sozluk
+class Klasor(Dosya):
+    def __init__(self, yol):
+        Dosya.__init__(self, yol)
+        self.klasorler = []
+        self.dosyalar = []
 
-ana_klasor = Directory("Mutant")
-print(ana_klasor.icindekiler())
+    def icerigi_oku(self):
+        for dosya_adi in os.listdir(self.kesin_yolu):
+            if os.path.isfile(os.path.join(self.kesin_yolu, dosya_adi)):
+                self.dosyalar.append(Dosya(os.path.join(self.kesin_yolu, dosya_adi)))
+            else:
+                k = Klasor(os.path.join(self.kesin_yolu, dosya_adi))
+                k.icerigi_oku()
+                self.klasorler.append(k)
+
+yedek_klasor = Klasor("MutantArchive")
+ana_klasor = Klasor("Mutant")
+ana_klasor.icerigi_oku()
+for dosya in ana_klasor.dosyalar:
+    try:
+        os.mkdir(os.path.join(yedek_klasor.kesin_yolu, dosya.ad))
+    except FileExistsError as hata:
+        pass
